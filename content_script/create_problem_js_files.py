a6931257c04e51f2fdf467fa561b684b8b0f2214
Shreya Bhandari <shreya.bhandari0220@gmail.com>
2024-08-15 02:19:07 -0400

    image changes

--- content_script/create_problem_js_files.py
+++ content_script/create_problem_js_files.py
@@ -28,7 +28,6 @@ def create_default_pathway(tutoring):
 
 
 def save_image(response, filename, path):
-    # Ensure the directory exists
     os.makedirs(path, exist_ok=True)
     file_path = os.path.join(path, filename)
     with open(file_path, 'wb') as outfile:
@@ -95,37 +94,8 @@ def save_images(images, checksums, path, num, old_path):
         if found:
             continue
 
-        try:
-        r = requests.get(i, headers=fake_headers)
-        r.raise_for_status()
-        if 'image' in r.headers['Content-Type']:
-            filename = os.path.basename(urlparse(i).path)
-            save_image(r, filename)
-        else:
-            print("Content fetched is not an image")
-    except (requests.exceptions.ConnectionError, ConnectionResetError) as exc:
-        parse_result = urlparse(i)
-        if bool(parse_result.query):
-            print("Image query params are not supported by the proxy. {}".format(i))
-            sys.exit(1)
-        new_i = "https://cdn.statically.io/img/{}{}".format(parse_result.netloc, parse_result.path)
-        print("Trying to proxy image {} with {}".format(i, new_i))
-        try:
-            r = requests.get(new_i, headers=fake_headers, timeout=10)
-            r.raise_for_status()
-            if 'image' in r.headers['Content-Type']:
-                filename = os.path.basename(urlparse(new_i).path)
-                save_image(r, filename)
-            else:
-                print("Content fetched via proxy is not an image")
-        except requests.exceptions.RequestException as e:
-            print("Error saving image with proxy: {}. Exception: {}".format(new_i, str(e)))
-    except requests.exceptions.RequestException as e:
-        print("Error saving image: {}. Exception: {}".format(i, str(e)))
-        continue
+        fetch_image(i)
 
-        with open(os.path.join(path, name), 'wb') as outfile:
-            outfile.write(r.content)
         updated_checksums.append(create_image_md5(os.path.join(path, name)))
 
     return names, num, " ".join(updated_checksums)
